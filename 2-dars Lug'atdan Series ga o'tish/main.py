c8c8469028d87ced7aaee05881a47e7447972d8c
Muhammadjon Ibrohimov <muhammadjon.dev.miu@gmail.com>
2025-02-27 10:11:57 +0500

    Series

--- 2-dars Lug'atdan Series ga o'tish/main.py
+++ 2-dars Lug'atdan Series ga o'tish/main.py
@@ -6,5 +6,29 @@ import numpy as np
 
 from pandas import Series
 
+cars_dict = {
+    'malibu' : 40000,
+    'lacetti' : 20000,
+    'toyota' : 45000,
+    'mazda' : 52000,
+    'honda' : 38000,
+}
 
+print(type(cars_dict))
 
+cars1 = Series(cars_dict)
+
+print('malibu' in cars1) # bir elementni Seriesda borligini tekshirish
+
+print(cars1['toyota']) # kalit so'z boyicha chaqirish
+
+# yangi indekslarni yaratib olish
+models = ['honda' , 'mazda' , 'lacetti' , 'jaguar' , 'malibu' , 'toyota' , 'bmw' , 'mercedes']
+
+# mavjud lug'atni Seriesga o'girish va uni indekslarnini almashtirish
+cars2 = Series(cars_dict , index = models)
+
+print(cars_dict) # oddiy lug'at
+print(cars2) # indekslari o'zgargan Series
+
+# yangi qo'shilgan indekslar NaN qiymatiga ega bo'ladi
---
+++ 3-dars Series metodlari/main.py
@@ -0,0 +1,75 @@
+# Series metodlari
+
+import pandas as pd
+import numpy as np
+from pandas import Series
+
+cars_dict = {
+    'malibu' : 40000,
+    'lacetti' : 20000,
+    'toyota' : 45000,
+    'mazda' : 52000,
+    'honda' : 38000,
+}
+
+models = ['honda' , 'mazda' , 'lacetti' , 'jaguar' , 'malibu' , 'toyota' , 'bmw' , 'mercedes']
+
+cars = Series(cars_dict , index = models)
+
+print(cars.isnull()) # Agar NaN bo'lsa True qaytaradi , aks holda Flase qaytaradi.
+
+print(cars.notnull()) # Agar NaN bo'lsa False qaytaradi , aks holda True qaytaradi
+cars1 = Series(cars_dict)
+
+print(cars1 + cars) # pandas o'zi mos indeksdagi qiymatlar ustida matemetik amallarni bajaradi
+
+
+# Metodlar
+
+
+cars.name = "Avtosalon" # butun boshli Seriesga nom berish
+
+cars.name = "Avtosalon" # butun boshli Seriesga nom berish
+
+print(cars.hasnans) # Butun boshli Seriesda NaN qiymatalr bormi yo'qmi bilib berai
+
+print(cars.dtype )# qiymatlarining ma'lumot turini ko'rsatadi
+
+print(cars1.is_unique) # takrorlanmas qiymatlar bor bolsa True , takrorlansa Flase
+
+print(cars.shape) # jami nechta ma'lumot borligini ko'rsatadi , hajm
+
+print(cars.size) # ma'lumotlar hajmi
+
+print(cars.iloc[0]) # ma'lumotlarga indekslarining tartib raqami bo'yicha murojaat qilish
+
+print(cars.loc['bmw']) # ma'lumotlarga indekslari orqali murojaat qilish
+
+print(cars.max()) # eng katta qiymatga murojaat qilish
+
+print(cars.min()) # eng kichik qiymatga murojaat qilish
+
+print(cars.mean()) # qiymatlarning o'rtacha raifmetigiga murojaat qilish
+
+fanlar = {
+    'matematika' : 'Abzalbek',
+    'fizika' : 'Niyozbek' ,
+    'adabiyot' : "Qodirbek" ,
+    'informatika' : 'Izzatbek'
+}
+
+subjects = Series(fanlar)
+
+print(subjects.loc['matematika']) # faqat bitta indeks
+
+print(subjects.iloc[[1 , 0 , 2]]) # bir nechta qiymatlarga indeksalrining tartib raqami bo'yicha murojaat qilisgh
+
+
+# String ma'lumotlarda .min va .max elemntlar aniqlanayotganaida ulardagi bosh hariflar (agar bir xil bo'lsa undan keyingilar)
+# bo'yicha solishtiriladi . Ular ASCII jadvalidagi soniga qarab aniqlanadi. .max ga jadval tepasida joylashganlari
+# . min ga esa jadval quyisida joylashganlari olinadi . Katta va kichik harf kelganida katta harf olinadi
+
+
+print(subjects.max())
+
+print(subjects.min())
